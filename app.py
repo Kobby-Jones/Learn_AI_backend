9c57be33986124498db78585e0c6ec60df12705e
Kobby-Jones <kobbyj099@gmail.com>
2026-06-23 06:56:44 +0000

    Configured port to run in production

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 """
 app.py — Main Flask application entry point
 """
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -54,4 +56,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
